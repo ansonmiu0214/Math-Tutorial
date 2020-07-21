c2b758ce89b0397563289b1e4811b814f5ca8afc
Anson Miu <me@ansonmiu.dev>
2020-07-21 13:48:31 +0800

    Separate visitor and parser from router

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,59 +1,19 @@
-import { ANTLRInputStream, CommonTokenStream, Token } from 'antlr4ts';
-import { arithmeticLexer } from './antlr/arithmeticLexer';
-import { arithmeticParser, ExpressionContext, AtomContext } from './antlr/arithmeticParser';
+import http from "http";
+import express from "express";
 
-// Create the lexer and parser
-const inputStream = new ANTLRInputStream("1 + 2 * (3 + 4)");
-const lexer = new arithmeticLexer(inputStream);
-const tokenStream = new CommonTokenStream(lexer);
-const parser = new arithmeticParser(tokenStream);
+const app = express();
+const server = http.createServer(app);
 
-// Parse the input, where `compilationUnit` is whatever entry point you defined
-const tree = parser.file();
+app.get("/_health", (req, res) => {
+  res.send("OK");
+})
 
-import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor'
-import { arithmeticVisitor } from './antlr/arithmeticVisitor';
+const PORT = Number(process.env.PORT ?? 5000);
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
 
-// Extend the AbstractParseTreeVisitor to get default visitor behaviour
-class EvalVisitor extends AbstractParseTreeVisitor<number> implements arithmeticVisitor<number> {
+// import { buildExprTree } from "./parser";
 
-  defaultResult() {
-    return 0
-  }
-
-  aggregateResult(aggregate: number, nextResult: number) {
-    return aggregate + nextResult
-  }
-  
-  visitExpression(context: ExpressionContext) {
-    if (context.TIMES()) {
-      return context.expression()
-      .map(child => super.visit(child))
-      .reduce((v1, v2) => v1 * v2);
-    }
-    // if (context.DIV()) {
-    //   const [v1, v2] = context.children!.map(super.visit);
-    //   return v1 / v2;
-    // }
-    if (context.PLUS()) {
-      return context.expression()
-        .map(child => super.visit(child))
-        .reduce((v1, v2) => v1 + v2);
-    }
-    if (context.MINUS()) {
-      return context.expression()
-        .map(child => super.visit(child))
-        .reduce((v1, v2) => v1 - v2);
-    }
-
-    return super.visitChildren(context);
-  }
-
-  visitAtom(context: AtomContext) {
-    return Number.parseInt(context.SCIENTIFIC_NUMBER().text);
-  }
-
-}
-
-const result = new EvalVisitor().visit(tree);
-console.log(result);
+// const tree = buildExprTree("1 + 2 * 3");
+// console.log(tree.toString());
---
+++ Server/src/parser/index.ts
@@ -0,0 +1,52 @@
+import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+import { arithmeticLexer } from '../antlr/arithmeticLexer';
+import { arithmeticParser, AtomContext, BinaryExprContext, UnaryExprContext, FileContext } from '../antlr/arithmeticParser';
+
+import { AbstractParseTreeVisitor } from 'antlr4ts/tree/AbstractParseTreeVisitor'
+import { arithmeticVisitor } from '../antlr/arithmeticVisitor';
+import { ExprNode, BinOp } from '../models/ExprNode';
+
+class ExprTreeVisitor extends AbstractParseTreeVisitor<ExprNode> implements arithmeticVisitor<ExprNode> {
+
+  defaultResult() {
+    return ExprNode.Val(0);
+  }
+
+  visitFile(ctx: FileContext) {
+    return super.visit(ctx.expression());
+  }
+
+  visitBinaryExpr(ctx: BinaryExprContext) {
+    const opToken = ctx.getChild(1).text;
+    const op = BinOp.fromToken(opToken);
+    if (!op) {
+      throw new Error(`Unsupported operand: ${op}`);
+    }
+
+    const leftChild = ctx.getChild(0);
+    const rightChild = ctx.getChild(2);
+    return ExprNode.BinExpr(super.visit(leftChild), super.visit(rightChild), op);
+  }
+
+  visitUnaryExpr(ctx: UnaryExprContext) {
+    return super.visit(ctx.expression());
+  }
+
+  visitAtom(ctx: AtomContext) {
+    return ExprNode.Val(Number.parseInt(ctx.text));
+  }
+
+}
+
+export function buildExprTree(expr: string) {
+  // Create the lexer and parser
+  const inputStream = new ANTLRInputStream(expr);
+  const lexer = new arithmeticLexer(inputStream);
+  const tokenStream = new CommonTokenStream(lexer);
+  const parser = new arithmeticParser(tokenStream);
+
+  // Parse the input
+  const tree = parser.file();
+
+  return new ExprTreeVisitor().visit(tree);
+}
